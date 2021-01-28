2126863c247f078b49107e896953301b517d3494
Vinicius A Santos <vinicius.andrade@riachuelo.com.br>
2021-01-28 09:33:29 -0300

    Setting Header Title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,8 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Vinícius A dos Santos";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle =
+    "Vinícius A Santos - Software Engineer & Solutions Architect";
 
 export default function Layout({ children, home }) {
     return (
