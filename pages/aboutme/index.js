37a04e32716b0535dcbddd6b500d5ca008054f3e
Vinicius A Santos <vinicius.andrade@riachuelo.com.br>
2021-02-02 09:00:31 -0300

    About me review

--- pages/aboutme/index.js
+++ pages/aboutme/index.js
@@ -23,16 +23,16 @@ export default function Post() {
             <section className={utilStyles.headingMd}>
                 <p>
                     Hey, welcome! I'm <b>Vinicius</b>. I'm a{" "}
-                    <b>Software Engineer</b> and <b>Solutions Architect</b>{" "}
-                    passionte about Software craftsmanship, desinging solutions
-                    and, of course, coding.
+                    <b>Software Engineer</b> and <b>Solutions Architect</b> who
+                    loves everything about Software craftsmanship, designing
+                    solutions and, of course, coding.
                 </p>
             </section>
 
             <section className={utilStyles.headingMd}>
                 <p>
                     My main skills are focused on <b>backend</b> development. In
-                    my day-to-day I'm working with{" "}
+                    my daily activities I play around with{" "}
                     <SkillsLabel>Java</SkillsLabel>,{" "}
                     <SkillsLabel>NodeJS</SkillsLabel>,{" "}
                     <SkillsLabel>SQL</SkillsLabel>,{" "}
@@ -42,41 +42,41 @@ export default function Post() {
                     <SkillsLabel>Cloud Computing (AWS)</SkillsLabel>,{" "}
                     <SkillsLabel>Terraform</SkillsLabel>
                     <br />
-                    But I also like playing arount with <b>
-                        frontend
-                    </b> using <SkillsLabel>React.JS</SkillsLabel> and{" "}
-                    <SkillsLabel>React Native</SkillsLabel>. By the way, this
-                    site is built over <SkillsLabel>Next.JS</SkillsLabel>
+                    But I also like <b>frontend</b> development and I usually
+                    choose <SkillsLabel>React.JS</SkillsLabel> and{" "}
+                    <SkillsLabel>React Native</SkillsLabel> for this. By the
+                    way, this site is built over{" "}
+                    <SkillsLabel>Next.JS</SkillsLabel>
                 </p>
             </section>
 
             <section className={utilStyles.headingMd}>
                 <p>
-                    Currently, I'm a <b>Technical Leader</b> at{" "}
+                    Currently, I'm a <b>Technical Leader</b> at the{" "}
                     <a href="https://www.riachuelo.com.br">
                         Riachuelo Fashion Stores
-                    </a>
-                    <br />I lead a team of 9 developers in a technical contexts.
-                    My main reponsibilities are:
+                    </a>{" "}
+                    and I'm <b>leading</b> a team of 9 developers in a technical
+                    way. My main responsibilities are:
                     <ul>
                         <li key="0">
-                            Design and explain <b>Architectural Solutions</b>
+                            Designing and explaining{" "}
+                            <b>Architectural Solutions</b>
                         </li>
                         <li key="1">
-                            Propose <b>standards</b> to be followed while we are
-                            coding
+                            Proposing <b>standards</b> to be followed while we
+                            are coding
                         </li>
                         <li key="2">
-                            Be aware of corporative{" "}
-                            <b>architectural principles and standards</b> and
-                            ensure that our team solutions agreed with it
+                            Ensuring that our solutions match the corporative{" "}
+                            <b>architectural principles and standards</b>
                         </li>
                         <li key="3">
-                            Bring standards and corporative solutions{" "}
-                            <b>together with other Technical Leaders</b>
+                            Bringing standards and corporative solutions{" "}
+                            <b>along with other Technical Leaders</b>
                         </li>
                         <li key="4">
-                            Manage my time to also code something, being a{" "}
+                            Managing my time to also code something, being a{" "}
                             <b>hands on</b> Technical Leader
                         </li>
                     </ul>
