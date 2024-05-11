e8a49df9ffe8b672fe234498562231e0e0fa3f1b
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-05-11 12:47:57 -0300

    List posts 1 per line

--- app/page.jsx
+++ app/page.jsx
@@ -8,14 +8,21 @@ const PostList = ({ className, children }) => {
     return <div className={className}>{children}</div>;
 };
 
-const PostListItem = ({ id, title, date, tags, backgroundColor = "white" }) => {
+const PostListItem = ({ id, title, date, tags, backgroundColor }) => {
     return (
-        <div className={`flex flex-col justify-between gap-2 w-full border border-slate-200 rounded-md bg-${backgroundColor} shadow-md p-4`}>
+        <div
+            className={`flex flex-col justify-between gap-2 w-full border border-slate-200 rounded-md ${
+                backgroundColor || "bg-white"
+            } shadow-sm p-4`}
+        >
             <span>
-                <Link href={`/posts/${id}`}>
+                <Link className="flex justify-between" href={`/posts/${id}`}>
                     <h2 className="text-lg font-bold text-slate-500 hover:underline">
                         {title}
                     </h2>
+                    <small className="text-xs font-light text-slate-400">
+                        <Date dateString={date} />
+                    </small>
                 </Link>
                 <div className="space-x-1.5">
                     {tags.map((tag) => (
@@ -23,9 +30,6 @@ const PostListItem = ({ id, title, date, tags, backgroundColor = "white" }) => {
                     ))}
                 </div>
             </span>
-            <small className="text-xs font-light text-slate-400">
-                <Date dateString={date} />
-            </small>
         </div>
     );
 };
@@ -35,13 +39,13 @@ export default function Home() {
     return (
         <section className="text-xl">
             <Header home={true} />
-            <PostList className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <PostList className="flex flex-col gap-3">
                 <PostListItem
                     id="the-biggest-challenge-i-faced-in-my-career"
                     title="The biggest challenge I've faced in my career"
                     date="2024-05-08"
                     tags={["career", "tech-lead"]}
-                    backgroundColor="yellow-50"
+                    backgroundColor="bg-yellow-50"
                 />
                 {allPostsData
                     .filter(
